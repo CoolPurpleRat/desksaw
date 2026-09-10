313a07b1cc76b527a717c33c53b8df8d22a6a807
CoolPurpleRat <adkivganov@gmail.com>
2026-09-10 16:08:03 +0300

    Optimization
    Reduced game loading time and first expie spawn time from total ~600ms to ~80ms. Reduced expie spawn time.

--- Scripts/commandsGlobal.gd
+++ Scripts/commandsGlobal.gd
@@ -41,14 +41,9 @@ func reload():
 
 
 func spawnExpie(petId: String = ""):
-	var path = "res://scenes/sawianBase.tscn"
-	var scene = load(path)
-	var instance = scene.instantiate()
+	var instance = preload("res://scenes/sawianBase.tscn").instantiate()
 
-	##newly spawned and without save data
-	var isNewborn:=false
 	if petId == "":
-		isNewborn=true
 		var skinName = GlobalVariable.userSkinPath.substr(0, len(GlobalVariable.userSkinPath) - 1)
 		skinName = skinName.substr(skinName.rfind("/") + 1)
 		petId = gbData.addPet(skinName)
@@ -58,13 +53,12 @@ func spawnExpie(petId: String = ""):
 	wrapper.scale = Vector2(4.0, 4.0)
 	
 	#Update scale based on saved info or spawn setting
-	if isNewborn:
+	if gbData.data["saw"][petId].has("spawnSize"):
+		wrapper.scale*=gbData.data["saw"][petId]["spawnSize"]
+	else:
 		if gbData.settings.has("spawnSize"):
 			wrapper.scale*=gbData.settings["spawnSize"]
-	else:
-		if gbData.data["saw"][petId].has("spawnSize"):
-			wrapper.scale*=gbData.data["saw"][petId]["spawnSize"]
-	
+		
 	get_tree().current_scene.add_child(wrapper)
 	wrapper.owner = get_tree().current_scene
 
--- Scripts/preload/SaveLoadSys.gd
+++ Scripts/preload/SaveLoadSys.gd
@@ -121,6 +121,8 @@ func addPet(skin: String = "Default") -> String:
 	data["saw"][newId]["skin"] = skin
 	if gbData.settings.has("spawnSize"):
 		data["saw"][newId]["spawnSize"]=gbData.settings["spawnSize"]
+	else:
+		data["saw"][newId]["spawnSize"]=1.0
 	
 	savetodisk(savePath, data)
 	return newId
